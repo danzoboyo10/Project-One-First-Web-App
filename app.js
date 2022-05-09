bd89d9acd4dfa9a07a0c60203328f6463994e8a0
Daniel Slebodnick <DanSlebodnick@Garys-MBP.cable.rcn.com>
2022-05-09 13:06:30 -0400

    Created a drop down menu

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const settings = {
 	"async": true,
 	"crossDomain": true,
-	"url": "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
+	"url": "https://exercisedb.p.rapidapi.com/exercises",
 	"method": "GET",
 	"headers": {
 		"X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
 		"X-RapidAPI-Key": "625c9ad15bmsh632c27ff42b138bp101e9cjsn25f93b4e0c16"
 	}
 };
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
