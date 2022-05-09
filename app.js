fa48d1f04fb91d7aa5a9750b7e53ee7dc8eacd02
Daniel Slebodnick <DanSlebodnick@Garys-MBP.cable.rcn.com>
2022-05-09 11:24:51 -0400

    WIP added APIand linked CSS,Js, and JQuery, to HTML

---
+++ app.js
@@ -0,0 +1,10 @@
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
+	"method": "GET",
+	"headers": {
+		"X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+		"X-RapidAPI-Key": "625c9ad15bmsh632c27ff42b138bp101e9cjsn25f93b4e0c16"
+	}
+};
