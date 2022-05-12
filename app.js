4f68b454e6a8cbf103d95150fe77f534d92c5cca
Daniel Slebodnick <DanSlebodnick@Garys-MBP.cable.rcn.com>
2022-05-12 13:03:16 -0400

    Almost finished just adding a bit more

--- app.js
+++ app.js
@@ -25,29 +25,40 @@ const $main = $('main')
 const $form = $('form')
 const $input = $(`input[type="text"]`)
 
-const $monday = $('#Mon')
-const $tuesday = $('#Tues')
-const $wednesday = $('#Wednes')
-const $thursday = $('#Thurs')
-const $friday = $('#Frid')
-const $saturday = $('#Sat')
-const $sunday = $('#Sund')
+const $monday = $('#mon')
+const $tuesday = $('#tues')
+const $wednesday = $('#wednes')
+const $thursday = $('#thurs')
+const $friday = $('#frid')
+const $saturday = $('#sat')
+const $sunday = $('#sund')
+
+$monday.on("click", function(){
+let workoutType = document.createElement("h2")
+
+    
+})
 
 
 
 
 //EVENT LISTENERS
 $form.on('submit', handleGetData)
-// $monday.on("click", handleGetData)
-// $tuesday.on("click", handleGetData)
-// $wednesday.on("click", handleGetData)
-// $thursday.on("click", handleGetData)
-// $friday.on("click", handleGetData)
-// $saturday.on("click", handleGetData)
-// $sunday.on("click", handleGetData)
+// $monday.on("click", displayWorkout)
+// $tuesday.on("click", displayWorkout)
+// $wednesday.on("click", displayWorkout)
+// $thursday.on("click", displayWorkout)
+// $friday.on("click", displayWorkout)
+// $saturday.on("click", displayWorkout)
+// $sunday.on("click", displayWorkout)
 
+ const subMenuBtn = document.querySelector(".dropdown-days")   
+ subMenuBtn.addEventListener("click", ()=>{
+     const workoutH2 = document.createElement("h2")
+     workoutH2.innerHTML = "Leg day"
 
-
+ })
+ 
 //FUNCTIONS
 function handleGetData(event) {
     event.preventDefault()
@@ -84,44 +95,27 @@ $.ajax(settings).then(function (data) {
         $("#content1").append(muscleTargEl)    
 })
 
-        $exerCssImage.html(function(){
         let exerCssDisplay = workout.gifUrl
         console.log(exerCssDisplay);  
-        let exerCssEl = document.createElement("img")
-        img.src = src
-        exerCssEl.innerText = exerCssDisplay;
-        $("#content1").append(exerCssEl)    
-})
+        $("#content1").append(`<img id="cssImg" src = "${exerCssDisplay}"/>`)    
 
 
-
-
-        
-        // $muscleTarg.text(workout.muscleTarg)
-        // console.log($exerName)
-        // $('content1').append(`<p> "${workout}"</p>`)
-        // console.log(userInput)
-        
     
-     console.log(data);
-    
-   
+     console.log(data); 
 
 }, 
  function (error) {
     console.log('something is wrong')
     console.log(error)
 })
+ 
 
 
 
 }
 
-
-
-
-// $("#content1").append(`<img src= "$(data.gifUrl"/>`)
-
-// let workoutImg = workout.
-
+// let muscleTargs = workout.target
+// console.log(muscleTargs);  
+// let muscleTargEl = document.createElement("li")
+// muscleTargEl.innerText = muscleTargs;
 
